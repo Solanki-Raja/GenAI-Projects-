ff89d7d6d44e0d85b23225756dd3b56cf93ad816
Solanki-Raja <112366148+Solanki-Raja@users.noreply.github.com>
2024-02-09 16:21:48 -0800

    Add files via upload

---
+++ vision.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import streamlit as st 
+import os
+import pathlib
+import textwrap
+
+from PIL import Image
+
+import google.generativeai as genai 
+
+from IPython.display import display
+from IPython.display import Markdown
+
+os.getenv("GOOGLE_API_KEY")
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+def get_gemini_response(question,image):
+    model= genai.GenerativeModel('gemini-pro-vision')
+    if question!="":
+        response= model.generate_content([question,image])
+    else:
+        response=model.generate_content(image)
+    return response.text
+
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini Application")
+
+input=st.text_input("Input: ",key="input")
+
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about the image")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input,image)
+    st.subheader("The Response is")
+    st.write(response)
